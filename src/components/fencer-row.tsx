cfccfd81203e5d20a7ad8caa005cf73828ee491a
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-07-31 17:02:29 -0300

    fixed team icon alignment

--- src/components/fencer-row.tsx
+++ src/components/fencer-row.tsx
@@ -1,25 +1,23 @@
-import FencerSummary from "~/models/FencerSummary";
-import { TableCell, TableRow } from "./ui/table";
-import TeamIcon from "./team-icon";
+import FencerSummary from '~/models/FencerSummary';
+import {TableCell, TableRow} from './ui/table';
+import TeamIcon from './team-icon';
 
-export default function FencerRow({ fencer }: { fencer: FencerSummary }) {
-  const teamIcon = `/team-icons/${fencer.universityId}.png`;
-  return (
-    <div className="flex flex-row items-center justify-between px-[16px] py-[8px]">
-      <div className="flex flex-row gap-1">
-        <div className="h-[24px] w-[24px] p-0">
-          <TeamIcon src={teamIcon} alt={teamIcon} />
+export default function FencerRow({fencer}: {fencer: FencerSummary}) {
+    const teamIcon = `/team-icons/${fencer.universityId}.png`;
+    return (
+        <div className="flex flex-row items-center justify-between px-[16px] py-[8px]">
+            <div className="flex flex-row gap-1">
+                <TeamIcon src={teamIcon} alt={teamIcon} />
+                <div className="h-fit p-0">{fencer.fullName}</div>
+            </div>
+            <div className="flex flex-row gap-1">
+                <div className="flex h-[28px] w-[28px] items-center justify-center rounded-md bg-green-400 font-semibold leading-none text-white">
+                    {fencer.record.wins}
+                </div>
+                <div className="flex h-[28px] w-[28px] items-center justify-center rounded-md bg-red-400 font-semibold leading-none text-white">
+                    {fencer.record.losses}
+                </div>
+            </div>
         </div>
-        <div className="h-fit p-0">{fencer.fullName}</div>
-      </div>
-      <div className="flex flex-row gap-1">
-        <div className="flex h-[28px] w-[28px] items-center justify-center rounded-md bg-green-400 font-semibold leading-none text-white">
-          {fencer.record.wins}
-        </div>
-        <div className="flex h-[28px] w-[28px] items-center justify-center rounded-md bg-red-400 font-semibold leading-none text-white">
-          {fencer.record.losses}
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/components/team-icon.tsx
+++ src/components/team-icon.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-export default function TeamIcon({ src, alt }: { src: string; alt: string }) {
-  return <Image src={src} alt={alt} width={24} height={24} />;
+export default function TeamIcon({src, alt}: {src: string; alt: string}) {
+    return (
+        <div className="flex h-[24px] w-[24px] items-center justify-center p-0">
+            <Image src={src} alt={alt} width={24} height={24} />
+        </div>
+    );
 }
