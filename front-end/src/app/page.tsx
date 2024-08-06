83da8013517340dd533cf2fa5d159568b38198a3
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-08-06 11:50:04 -0300

    changed padding on home page

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -38,7 +38,7 @@ export default async function HomePage() {
         },
     ];
     return (
-        <main className="p-6">
+        <main className="px-6">
             <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
                 {tables.map((data) => (
                     <Link href={data.url} key={data.title}>
