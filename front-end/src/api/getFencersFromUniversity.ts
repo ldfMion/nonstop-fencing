7e2631c52067390709482ac2f3e595006dedfd06
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-08-29 20:08:18 -0400

    sorted fencers in university page

--- front-end/src/api/getFencersFromUniversity.ts
+++ front-end/src/api/getFencersFromUniversity.ts
@@ -3,5 +3,7 @@ import getRecordsfromCsv from '../../helpers/getRecordsFromCsv';
 
 export default async function getFencersFromUniversity(universityId: string, team: Team) {
     const data = await getRecordsfromCsv();
-    return data.filter((fencer) => fencer.universityId === universityId && fencer.team === team);
+    return data
+        .filter((fencer) => fencer.universityId === universityId && fencer.team === team)
+        .sort((a, b) => b.rating - a.rating);
 }
