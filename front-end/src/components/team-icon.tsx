5480bdf55036bac7c9b152a579847a19bd534c34
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-09-03 21:21:09 -0400

    added link to icons

--- front-end/src/components/team-icon.tsx
+++ front-end/src/components/team-icon.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import {cn} from '~/lib/utils';
 
 export default function TeamIcon({universityId, className}: {universityId: string; className?: string}) {
     const url = `/team-icons/${universityId}.png`;
     return (
-        <div className={cn('relative flex items-center justify-center p-0', className)}>
-            <Image src={url} alt={''} width={100} height={100} className={`relative h-full w-full object-contain`} />
-        </div>
+        <Link href={`/mens/universities/${universityId}`}>
+            <div className={cn('relative flex items-center justify-center p-0', className)}>
+                <Image src={url} alt={''} width={100} height={100} className={`relative h-full w-full object-contain`} />
+            </div>
+        </Link>
     );
 }
